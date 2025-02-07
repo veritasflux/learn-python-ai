093ed63b69991fdf2f00f98496e91c2f85c7a314
Amine Brini <60950180+veritasflux@users.noreply.github.com>
2025-02-07 09:34:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import os
 from groq import Groq
 
 client = Groq(api_key=os.getenv("GROQ_API_TOKEN"))
-def generate_lesson(user_input):
+
+def generate_lesson():
     completion = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
         messages=[
